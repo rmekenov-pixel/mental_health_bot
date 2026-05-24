faa6302cd09df834b63bcf3c8a10d6e983c3db9f
rmekenov-pixel <rmekenov@gmail.com>
2026-05-24 12:02:26 +0500

    Fix env variables reading

--- bot/config.py
+++ bot/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./mental_health.db")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./mental_health.db")
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
