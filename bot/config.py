985b087ef39d3e48478b5e2d7d89cc7e0183da20
rmekenov-pixel <rmekenov@gmail.com>
2026-05-28 15:43:06 +0500

    Debug token

--- bot/config.py
+++ bot/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv(override=False)
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8992809126:AAG2D9UMhcznMAF8w_d_nHsXXcmRNHVBZ-0")
 DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./mental_health.db")
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
