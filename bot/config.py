66fb2d9adba65e3b163aaeffd94e55cc3ec340fc
rmekenov-pixel <rmekenov@gmail.com>
2026-05-28 15:32:02 +0500

    Fix env variable override issue

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=False)
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./mental_health.db")
